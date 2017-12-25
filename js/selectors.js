48cf25cbf4ae7c3c7a36f4f54f9cd35860745133
jencekade <gaephuoc@gmail.com>
2017-12-25 08:49:28 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,23 @@
 // declare your functions here...
+function paragraphSelector() {
+  const paragraphs = $('p');
+  return paragraphs;
+}
+
+function lastImageSelector() {
+  const lastImage = $('img:last');
+  return lastImage;
+}
+
+function ninjaBabySelector() {
+  const babyImage = $('#baby-ninja'); 
+  return babyImage;
+}
+
+function divSelector() {
+
+}
+
+function firstListItem() {
+
+}
